c39c828786cdab4b9bc0d33361bdb5bfedc65f6d
MuellerChr2 <MuellerChr2@mykaefer.com>
2025-03-07 11:53:02 +0100

    Refactor Nginx configuration for improved SSL and LAN support
    - Split Nginx configuration into SSL and non-SSL server blocks
    - Add DISABLE_HTTPS_REQUIREMENT environment variable
    - Improve gzip and proxy settings
    - Remove deprecated SSL protocol configuration

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -9,6 +9,7 @@ LAN_ADDRESS=$(yq e .vaultwarden-lan-address /data/start9/config.yaml)
 cat << EOF >> /.env
 PASSWORD_ITERATIONS=2000000
 DOMAIN="https://$TOR_ADDRESS"
+DISABLE_HTTPS_REQUIREMENT=true
 EOF
 
 cat << EOF > /data/start9/stats.yaml
@@ -39,14 +40,10 @@ EOF
 
 CONF_FILE="/etc/nginx/http.d/default.conf"
 NGINX_CONF='
+# SSL Server Block
 server {
-    ##
-    # `gzip` Settings
-    #
-    #
     gzip on;
     gzip_disable "msie6";
-
     gzip_vary on;
     gzip_proxied any;
     gzip_comp_level 6;
@@ -73,34 +70,88 @@ server {
     text/javascript
     text/plain
     text/xml;
+
     listen 3443 ssl;
-    listen 8080;
     http2 on;
     ssl_certificate /mnt/cert/main.cert.pem;
     ssl_certificate_key /mnt/cert/main.key.pem;
-    server_name  localhost;
+    server_name localhost;
     client_max_body_size 128M;
 
     location / {
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Proto $scheme;
-      proxy_set_header X-Forwarded-Host $host;
-      set_real_ip_from 0.0.0.0/0;
-      proxy_redirect off;
-      proxy_pass http://0.0.0.0:80;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        set_real_ip_from 0.0.0.0/0;
+        proxy_redirect off;
+        proxy_pass http://0.0.0.0:80;
     }
+
     location /notifications/hub {
-      proxy_http_version 1.1;
-      proxy_set_header Upgrade $http_upgrade;
-      proxy_set_header Connection "upgrade";
-      proxy_pass http://0.0.0.0:80;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_pass http://0.0.0.0:80;
+    }
+}
+
+# Non-SSL Server Block for LAN
+server {
+    gzip on;
+    gzip_disable "msie6";
+    gzip_vary on;
+    gzip_proxied any;
+    gzip_comp_level 6;
+    gzip_buffers 16 8k;
+    gzip_http_version 1.1;
+    gzip_min_length 256;
+    gzip_types
+    application/atom+xml
+    application/geo+json
+    application/javascript
+    application/x-javascript
+    application/json
+    application/ld+json
+    application/manifest+json
+    application/rdf+xml
+    application/rss+xml
+    application/xhtml+xml
+    application/xml
+    font/eot
+    font/otf
+    font/ttf
+    image/svg+xml
+    text/css
+    text/javascript
+    text/plain
+    text/xml;
+
+    listen 8080;
+    server_name localhost;
+    client_max_body_size 128M;
+
+    location / {
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        set_real_ip_from 0.0.0.0/0;
+        proxy_redirect off;
+        proxy_pass http://0.0.0.0:80;
+    }
+
+    location /notifications/hub {
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_pass http://0.0.0.0:80;
     }
 }
 '
 echo "$NGINX_CONF" > $CONF_FILE
-sed -i "s#ssl_protocols TLSv1.1#ssl_protocols#g" /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;' &
 exec  tini -p SIGTERM -- /start.sh
